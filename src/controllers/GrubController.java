1df7192c1d9a094dc3350c3bfc9b8f35fc80c96c
jleupp <jeffrey.leupp@gmail.com>
2016-02-15 10:37:44 -0700

    bringing working_master to same

--- src/controllers/GrubController.java
+++ src/controllers/GrubController.java
@@ -1,17 +1,14 @@
 package controllers;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import data.GrubRestaurantDAO;
+import data.GrubDAO;
 
 @Controller
 public class GrubController {
